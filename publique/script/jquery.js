e18828428e999aa5d516b5d1572a229939f1abcc
Perriguey <yo44prg@icloud.com>
2020-01-28 14:19:07 +0100

    jquery

---
+++ publique/script/jquery.js
@@ -0,0 +1,27 @@
+function submit() {
+    let apiUrl = "https://slack.com/api/chat.postMessage";
+    let token = "xoxb-927063493862-924428453572-KcxxtatfIAdX5f2X4DVMsxUh";
+    let channel = "#bot";
+    let prenom = document.getElementById('prenomSlack').value;
+    let msg = document.getElementById('commentSlack').value;
+    let mail = document.getElementById('emailSlack').value;
+    let user = "yo44prg";
+
+    $.ajax({
+        data: {
+            "token": token,
+            "channel": channel,
+            "text": prenom + " " + mail + " " + msg,
+            "as_user": user,
+        },
+        dataType: 'text',
+        type: 'POST',
+        url: apiUrl,
+        error: function (error) {
+            alert("error: " + error);
+        },
+        success: function (data) {
+            console.log("result: " + data)
+        }
+    });console.log('plop');
+}
