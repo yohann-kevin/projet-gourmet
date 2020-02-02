6f47b39ddaaf289e7a7a1a79c013ba8b8c4d68d5
Perriguey <yo44prg@icloud.com>
2020-02-02 20:48:58 +0100

    googleConnect

--- publique/script/index.js
+++ publique/script/index.js
@@ -123,3 +123,15 @@ function search() {
             }
         })
 }
+// function onSignIn(googleUser) {
+//   var profile = googleUser.getBasicProfile();
+//   console.log("ID: " + profile.getId()); 
+//   console.log('Full Name: ' + profile.getName());
+//   console.log('Given Name: ' + profile.getGivenName());
+//   console.log('Family Name: ' + profile.getFamilyName());
+//   console.log("Image URL: " + profile.getImageUrl());
+//   console.log("Email: " + profile.getEmail());
+
+//   var id_token = googleUser.getAuthResponse().id_token;
+//   console.log("ID Token: " + id_token);
+// }
